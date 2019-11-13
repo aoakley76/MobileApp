a0c1d87fc8e57dfb477e04c33af8a1e90fe06055
aoakley76 <56124900+aoakley76@users.noreply.github.com>
2019-11-13 15:28:13 +0000

    Add files via upload

--- LoginCW/app/src/main/java/com/example/logincw/MainActivity.java
+++ LoginCW/app/src/main/java/com/example/logincw/MainActivity.java
@@ -7,12 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper db;
     EditText editEmail, editPassword;
-    Button btnLogin, btnRegister;
+    Button btnLogin, btnRegister, btnForgottenPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
             // opens register screen
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegisterScreen.class));
+            }
+        });
+
+        btnForgottenPassword = (Button) findViewById(R.id.btnForgottenPW);
+        btnForgottenPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            // opens login screen
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ForgottenPassword.class));
             }
         });
     }}
--- LoginCW/app/src/main/java/com/example/logincw/RegisterScreen.java
+++ LoginCW/app/src/main/java/com/example/logincw/RegisterScreen.java
@@ -2,13 +2,26 @@ package com.example.logincw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterScreen extends AppCompatActivity {
-
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
-    }
-}
+
+        btnBack = (Button) findViewById(R.id.button_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+        @Override
+
+        // opens login screen
+        public void onClick(View v) {
+            startActivity(new Intent(RegisterScreen.this, MainActivity.class));
+        }
+    });
+}}
+
