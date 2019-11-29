ff05df39f8fce6944b814345cdd0f31b93c9434e
aoakley76 <56124900+aoakley76@users.noreply.github.com>
2019-11-29 00:41:59 +0000

    Update MainActivity.java

--- LoginCW/app/src/main/java/com/example/logincw/MainActivity.java
+++ LoginCW/app/src/main/java/com/example/logincw/MainActivity.java
@@ -3,43 +3,91 @@ package com.example.logincw;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper db;
-    EditText editEmail, editPassword;
-    Button btnLogin, btnRegister, btnForgottenPassword;
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText editEmail, editPassword;
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
 
-        db = new DatabaseHelper(this);
+        mAuth = FirebaseAuth.getInstance();
 
-        editEmail = (EditText) findViewById(R.id.editEmail);
-        editPassword = (EditText) findViewById(R.id.editPassword);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in " + user.getUid());
+                    toastMessage("Successfully signed in with " + user.getEmail());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
 
-        btnRegister = (Button) findViewById(R.id.btnRegister);
+                }
+            }
+        };
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-
-            // opens register screen
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegisterScreen.class));
             }
         });
 
-        btnForgottenPassword = (Button) findViewById(R.id.btnForgottenPW);
-        btnForgottenPassword.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-
-            // opens login screen
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ForgottenPassword.class));
+                String email = editEmail.getText().toString();
+                String pass = editPassword.getText().toString();
+
+                startActivity(new Intent(MainActivity.this, HomeScreen.class));
+
+                if (!email.equals("") && !pass.equals("")) {
+                    mAuth.signInWithEmailAndPassword(email, pass);
+                } else {
+                    toastMessage("You didn't fill in all the fields");
+                }
             }
         });
-    }}
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
