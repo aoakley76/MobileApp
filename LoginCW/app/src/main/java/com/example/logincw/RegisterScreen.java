ebb8d74b2f4b8616adff322c15a4f2b9f58de5cd
aoakley76 <56124900+aoakley76@users.noreply.github.com>
2019-11-29 00:45:35 +0000

    Update RegisterScreen.java

--- LoginCW/app/src/main/java/com/example/logincw/RegisterScreen.java
+++ LoginCW/app/src/main/java/com/example/logincw/RegisterScreen.java
@@ -1,27 +1,69 @@
-package com.example.logincw;
+{
 
-import androidx.appcompat.app.AppCompatActivity;
+    private FirebaseAuth mAuth;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+    private EditText editEmail, editPassword;
+    private Button btnBack, btnReg;
 
-public class RegisterScreen extends AppCompatActivity {
-    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
 
-        btnBack = (Button) findViewById(R.id.button_back);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnReg = findViewById(R.id.buttonReg);
+        btnBack = findViewById(R.id.button_back);
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
-        @Override
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterScreen.this, MainActivity.class));
+            }
+        });
 
-        // opens login screen
-        public void onClick(View v) {
-            startActivity(new Intent(RegisterScreen.this, MainActivity.class));
-        }
-    });
-}}
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String email = editEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterScreen.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(RegisterScreen.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(RegisterScreen.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(RegisterScreen.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
+            }
+        });
+    }
+}
